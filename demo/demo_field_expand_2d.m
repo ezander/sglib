04ccb533e5d94df176ee2d6cabd7635d82bedef8
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-03-26 14:48:55 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@9 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- demo/demo_field_expand_2d.m
+++ demo/demo_field_expand_2d.m
@@ -3,19 +3,34 @@
 init_demos
 
 clear
-%s=load('mesh/rect_mesh_coarse.mat');
-s=load('data/rect_mesh.mat');
-els=s.nodes;
-pos=s.coords;
+if false
+    %s=load('data/rect_mesh_coarse.mat');
+    s=load('data/rect_mesh.mat');
+    %s=load('data/lshape_coarse.mat');
+    %s=load('data/lshape_medium.mat');
 
-[els,pos]=correct_mesh( els, pos );
-M=mass_matrix( els, pos );
+    els=s.nodes;
+    pos=s.coords;
+    [els,pos]=correct_mesh( els, pos );
+    trimesh( els, pos(:,1), pos(:,2) );
+    drawnow;
 
+    M=mass_matrix( els, pos );
+else
+    geom='lshapeg';
+    [p,e,t]=initmesh(geom);
+    [p,e,t]=refinemesh(geom,p,e,t); 
+    pdemesh(p,e,t);
+    [K,M,F]=assema(p,t,0,1,0);
+    els=t(1:3,:)';
+    pos=p';
+    %return
+end
 
 % expansion of the right hand side field (f)
 p_f=3;
 m_gam_f=22;
-m_f=22;
+m_f=12;
 lc_f=[0.03 0.2]; %#ok
 lc_f=0.2; %#ok
 h_f={@beta_stdnor,{4,2}}; %#ok
@@ -32,22 +47,21 @@ tic
 toc
 disp( 'performing kl expansion, this may take a while, too ...' );
 tic
-[mu_f,f_i_alpha,v_f]=pce_to_kl( f_alpha, I_f, M, m_f );
+[mu_f,f_i_alpha,v_f,relerr]=pce_to_kl( f_alpha, I_f, m_f, M, [] );
 toc
-
-
-Bei Floerke anrufen!!!!
-
+fprintf( 'relative error computing KL: %g\n', relerr );
 
 clf;
 for i=1:12
     subplot(4,4,i);
     plot_field( els, pos, v_f(:,i) );
+    title(sprintf('KLE: f_%d',i));
 end
 for i=13:16
     subplot(4,4,i);
     f_ex=kl_pce_field_realization( pos, mu_f, v_f, f_i_alpha, I_f );
     plot_field( els, pos, f_ex );
+    title(sprintf('Sample: %d',i-12));
 end
 
 disp( 'the first three rows shows the KL eigenfunctions of the field' );
