a7063125ed080803ebaaf5bf1b8f3a3e7fe8b3d1
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-04-04 15:24:52 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@34 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- demo/iciam/show_in_out_samples.m
+++ demo/iciam/show_in_out_samples.m
@@ -3,5 +3,3 @@ clf;
 subplot(1,3,1); plot_pce_realizations_1d( x, k_alpha, I_k, 'realizations', n, 'show_stat', 3 );
 subplot(1,3,2); plot_pce_realizations_1d( x, f_alpha, I_f, 'realizations', n, 'show_stat', 3 );
 subplot(1,3,3); plot_pce_realizations_1d( x, u_alpha, I_u, 'realizations', n, 'show_stat', 3 );
-
-%%
