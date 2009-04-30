9a73b6f692869c3c91d00ab975fd40702bac32de
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-04-30 14:58:27 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@48 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- apply_linear_operator.m
+++ apply_linear_operator.m
@@ -1,13 +1,16 @@
 function b=apply_linear_operator( A, x )
 
-sizeonly=ischar(x) && strcmp(x,'size');
+if nargin<2
+    x=[];
+end
+sizeonly=isempty( x );
 
 if isnumeric(A)
     % assume A is a matrix 
     if sizeonly
-        b=A*x;
-    else
         b=size(A);
+    else
+        b=A*x;
     end
 elseif isfunction(A)
     % A is an operator (size query must be handled by A)
---
+++ test_apply_linear_operator.m
@@ -0,0 +1,45 @@
+function test_apply_linear_operator
+% TEST_APPLY_LINEAR_OPERATOR Test the APPLY_LINEAR_OPERATOR function.
+%
+% Example 
+%    test_apply_linear_operator
+%
+% See also TESTSUITE
+
+%   Elmar Zander
+%   Copyright 2007, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+assert_set_function( 'apply_linear_operator' );
+
+
+% there is not much point in bigger matrices here, just check that basic
+% functionality works
+M=[1, 2; 3, 4];
+x=[1; 5];
+y=M*x;
+
+assert_equals( apply_linear_operator( M ), [2,2], 'M_size' );
+assert_equals( apply_linear_operator( M, [] ), [2,2], 'M_size' );
+assert_equals( apply_linear_operator( M, x ), y, 'M' );
+
+func={ @doit, {M}, {1} };
+assert_equals( apply_linear_operator( func ), [2,2], 'func_size' );
+assert_equals( apply_linear_operator( func, [] ), [2,2], 'func_size' );
+assert_equals( apply_linear_operator( func, x ), y, 'func' );
+
+function y=doit( M, x )
+if isempty(x)
+    y=size(M);
+else
+    y=M*x;
+end
