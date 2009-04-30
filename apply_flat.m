fed73c168f02ad328db5e6d375be1489cd2d56fb
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-04-30 14:45:36 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@47 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- apply_flat.m
+++
@@ -1,6 +0,0 @@
-function y=apply_flat(A,x,shape)
-xs=reshape(x,shape);
-ys=apply_stochastic_operator(A,xs);
-y=reshape(ys,size(x));
-
-%%
--- apply_flat_reduce.m
+++
@@ -1,7 +0,0 @@
-function y=apply_flat_reduce(A,x,shape,m)
-xs=reshape(x,shape);
-ys=apply_stochastic_operator(A,xs);
-ys=truncated_svd(x,m);
-y=reshape(ys,size(x));
-
-%%
