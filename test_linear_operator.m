b78e94b02f0bc6c4e88a83e03645faa54a7fe231
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-05-20 14:48:07 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@67 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- test_linear_operator.m
+++ test_linear_operator.m
@@ -14,24 +14,37 @@ assert_set_function( 'apply_linear_operator' );
 M=[1, 2; 3, 4; 5, 10];
 x=[1; 5];
 y=M*x;
+s=size(M);
+linop1=linear_operator( M );
+linop2={ size(M), {@mtimes, {M}, {1} } };
+linop3={ linop2{:}, {@mldivide, {M}, {1} } };
 
-assert_equals( linear_operator_size( M ), [3,2], 'M_size' );
-assert_equals( linear_operator_apply( M, x ), y, 'M' );
+assert_equals( linear_operator_size( M ), s, 'M_size' );
+assert_equals( linear_operator_size( linop1 ), s, 'lo1_size' );
+assert_equals( linear_operator_size( linop2 ), s, 'lo2_size' );
+assert_equals( linear_operator_size( linop3 ), s, 'lo3_size' );
 
-func={ @doit, {M}, {1} };
-assert_equals( linear_operator_size( func ), [3,2], 'func_size' );
-assert_equals( apply_linear_operator_apply( func, x ), y, 'func' );
-
-func={ @apply_linear_operator, {M}, {1} };
-assert_equals( linear_operator_size( func ), [3,2], 'rec_size' );
-assert_equals( linear_operator_apply( func, x ), y, 'rec' );
+assert_equals( linear_operator_apply( M, x ), y, 'M_apply' );
+assert_equals( linear_operator_apply( linop1, x ), y, 'lo1_apply' );
+assert_equals( linear_operator_apply( linop2, x ), y, 'lo2_apply' );
+assert_equals( linear_operator_apply( linop3, x ), y, 'lo3_apply' );
 
 
-function y=doit( M, x )
-if isempty(x)
-    y=size(M);
-else
-    y=M*x;
-end
+% compose
 
+
+
+
+M=[1, 2, 3; 3, 4, 6; 5, 10, 14];
+x=[1; 5; 7];
+y=M*x;
+s=size(M);
+linop1=linear_operator( M );
+linop2={ size(M), {@mtimes, {M}, {1} } };
+linop3={ linop2{:}, {@mldivide, {M}, {1} } };
+
+assert_equals( linear_operator_solve( M, y ), x, 'M_solve' );
+assert_equals( linear_operator_solve( linop1, y ), x, 'lo1_solve' );
+assert_equals( linear_operator_solve( linop2, y ), x, 'lo2_solve' );
+assert_equals( linear_operator_solve( linop3, y ), x, 'lo3_solve' );
 
