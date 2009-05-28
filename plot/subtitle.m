df94167f34f402194b5e17a679c7f3ae5da498a0
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-05-28 15:00:05 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@80 ad3af975-698c-43d8-bc0c-eed00f6a1e86

---
+++ plot/subtitle.m
@@ -0,0 +1,43 @@
+function subtitle( str, varargin )
+% SUBTITLE Create a title over multiple subplots.
+%   SUBTITLE(STR, VARARGIN) plots a title over all subplots generated with
+%   the SUBPLOT command (in contrast the TITLE command only makes a title
+%   for each subplot separately). 
+%
+% Options:
+%   position:  {0.97}
+%     relative vertical position for the display of the title. 1.0 is
+%     topmost, 0.0 bottommost, the default is hopefully a good compromise.
+% 
+% Example (<a href="matlab:run_example subtitle">run</a>)
+%     x=linspace(0,2*pi);
+%     clf;
+%     subplot(1,2,1); plot(x,sin(x)); title('Sin'); axis square;
+%     subplot(1,2,2); plot(x,cos(x)); title('Cos'); axis square;
+%     subtitle('Trigonometric functions');
+% 
+% See also SUBPLOT, TITLE
+
+%   Elmar Zander
+%   Copyright 2009, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+options=varargin2options( varargin{:} );
+[position,options]=get_option( options, 'position', 0.97 );
+check_unsupported_options( options, mfilename );
+
+ha=findall( gcf, 'Tag', 'SubTitle' );
+if ~isempty(ha)
+    delete(ha);
+end
+axes('Position', [0, 0, 1, position], 'Xlim',[0, 1],'Ylim',[0, 1],'Box','off','Visible','off','Units', 'normalized', 'clipping', 'off', 'Tag', 'SubTitle');
+
+text( 0.5, 1, str ,'HorizontalAlignment', 'center', 'VerticalAlignment', 'top', 'Interpreter', 'latex' );
---
+++ util/test_ismatoct.m
@@ -0,0 +1,32 @@
+function test_ismatoct
+% TEST_ISMATOCT Test the IS_MATLAB/IS_OCTAVE functions.
+%
+% Example (<a href="matlab:run_example test_ismatoct">run</a>) 
+%    test_ismatoct
+%
+% See also IS_MATLAB, IS_OCTAVE, TESTSUITE
+
+%   Elmar Zander
+%   Copyright 2007, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+assert_set_function( 'ismatlab' );
+
+isml=ismatlab;
+assert_true( isml==true || isml==false, 'function must return a boolean value' );
+
+assert_set_function( 'isoctave' );
+
+isoct=isoctave;
+assert_true( isoct==true || isoct==false, 'function must return a boolean value' );
+
+assert_true( isoct~=isml, 'is_matlab and is_octave must return different values' );
