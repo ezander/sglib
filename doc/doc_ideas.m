e3fc65eb713c3cf9f0a85cfeb6008a38e944d045
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-06-23 08:22:30 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@143 ad3af975-698c-43d8-bc0c-eed00f6a1e86

---
+++ doc/doc_ideas.m
@@ -0,0 +1,7 @@
+% DOC_IDEAS Contains some ideas that are underlying or should be implemented in SGLIB.
+%   
+%   * Idea 1: Freedom of the user: use whatever other framework you like,
+%             make everything in and out explicit
+%   * Idea 2: Separate functions into public and private interface. Public
+%             has all the option passing and consistency checking, and in
+%             private is the pure algorithm.
