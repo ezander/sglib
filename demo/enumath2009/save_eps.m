720c30ac406fd8a0014260fa988dc87a166607cc
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-06-26 11:57:06 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@152 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- demo/enumath2009/save_eps.m
+++ demo/enumath2009/save_eps.m
@@ -1,4 +1,14 @@
 function save_eps( basename, topic );
 
-filename=sprintf( '%s_%s.eps', basename, topic );
+dir='eps';
+[success,message,messageid]=mkdir(dir);
+if ~success
+    error( messageid, 'mkdir %s did not succeed: %s', dir, message);
+end
+if ~exist('eps','dir')
+    error( '%s exists but is not a directory', dir );
+end
+
+filename=sprintf( './%s/%s_%s.eps', dir, basename, topic );
 print( filename, '-depsc2' );
+disp(filename);
--- demo/enumath2009/tensor_methods_1d_conv.m
+++ demo/enumath2009/tensor_methods_1d_conv.m
@@ -16,18 +16,15 @@ basename=sprintf(basename, params{:});
 
 %% KL eigenfunctions
 plot(pos,u_i_k); 
-title('KL eigenfunctions of $u$', latex_opts{:});
-title('KL eigenfunctions of $u$', tex_opts{:});
-title('KL eigenfunctions of $u$', 'Interpreter', 'latex');
+title('KL eigenfunctions of u');
 save_eps( basename, 'kl_eigenfuncs' );
-print( 'rf_u_kl_eig.eps', '-depsc' );
+userwait;
 
 %% Solution and realizations
 plot_kl_pce_realizations_1d( pos, mu_u_i, u_i_k, u_k_alpha, I_u, 'realizations', 50 );
-title('mean/var/samples of $u$', text_opts{:});
-print( 'rf_u_kl_real.eps', '-depsc' );
-
-
+title('mean/var/samples of $u$');
+save_eps( basename, 'solution' );
+userwait;
 
 %% Error and truncation
 clf;
@@ -35,19 +32,20 @@ n=1:length(res);
 tol=cell2mat({res(n).tol});
 relerr=cell2mat({res(n).relerr});
 plot( logscale(tol,'cutoff',1e-15), logscale(relerr,'cutoff',1e-15), '-x' );
-xlabel('log_{10}(\epsilon)', tex_opts{:}); 
-ylabel('log_{10}(E)', text_opts{:});
-print( 'pcg_conv1_n_eps.eps', '-depsc' );
+xlabel('log_{10}(\epsilon)'); 
+ylabel('log_{10}(E)');
+save_eps( basename, 'error_over_trunc' );
 userwait;
+
 
 %% Numerical rank 
 tol=cell2mat({res(n).tol});
 k=cell2mat({res(n).k});
 plot( logscale(tol), k, 'x-' ); 
 title( 'Numerical rank' );
-xlabel( 'log_{10}(\epsilon)', text_opts{:} );
-ylabel( 'k', text_opts{:} );
-print( 'pcg_conv1_numrank_over_eps.eps', '-depsc' );
+xlabel( 'log_{10}(\epsilon)');
+ylabel( 'k' );
+save_eps( basename, 'num_rank' );
 userwait;
 
 %% Number of iterations
@@ -58,12 +56,16 @@ ylim([0,max(iter)]);
 title( 'Iterations' );
 xlabel( 'log_{10}(\epsilon)' );
 ylabel( 'n' );
+save_eps( basename, 'num_iter' );
+userwait;
 
 %% Update ratio 2D
 val={}; for i=n; val={val{:}, res(i).info.update_ratio}; end
 tol={res.tol};
 iteration_plot( val, tol,  'Update ratio', '\tau', [], 0.002 );
 ylim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_2d' );
+userwait;
 
 
 %% Update ratio 3D
@@ -71,7 +73,8 @@ delta=0.2;
 val={}; for i=n; val={val{:}, max(min(res(i).info.update_ratio,1+delta),-delta)}; end
 tol={res.tol};
 iteration_plot3( val, tol,  'Update ratio', '\tau' );
-%zlim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_3d' );
+userwait;
 
 %% Update ratio 1
 i=1;
@@ -79,6 +82,8 @@ val={res(i).info.update_ratio};
 tol={res(i).tol};
 iteration_plot( val, tol,  'Update ratio', '\tau' );
 ylim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_E1' );
+userwait;
 
 %% Update ratio 3
 i=3;
@@ -86,6 +91,8 @@ val={res(i).info.update_ratio};
 tol={res(i).tol};
 iteration_plot( val, tol,  'Update ratio', '\tau' );
 ylim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_E3' );
+userwait;
 
 %% Update ratio 5
 i=5;
@@ -93,23 +100,31 @@ val={res(i).info.update_ratio};
 tol={res(i).tol};
 iteration_plot( val, tol,  'Update ratio', '\tau' );
 ylim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_E5' );
+userwait;
 
-%% Update ratio 5
+%% Update ratio 6
 i=6;
 val={res(i).info.update_ratio};
 tol={res(i).tol};
 iteration_plot( val, tol,  'Update ratio', '\tau' );
 ylim([-0.1,1.1]);
+save_eps( basename, 'update_ratio_E6' );
+userwait;
 
 %% Relative residual
 val={}; for i=n; val={val{:}, logscale(res(i).info.res_relnorm)}; end
 tol={res.tol};
 iteration_plot( val, tol,  'Relative residual norm', '||r_\epsilon||/||r_0||', -.3 );
+save_eps( basename, 'rel_residual' );
+userwait;
 
 %% Relative error
 val={}; for i=n; val={val{:}, logscale(res(i).info.sol_relerr)}; end
 tol={res.tol};
 iteration_plot( val, tol,  'Relative error norm', '||u_\epsilon-u||/||u||', -.3 );
+save_eps( basename, 'rel_error' );
+userwait;
 
 
 
