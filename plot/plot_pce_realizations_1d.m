3d5ce873432da2ac96c592c4554cce59c029859d
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-07-16 12:01:59 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@162 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- plot/plot_pce_realizations_1d.m
+++ plot/plot_pce_realizations_1d.m
@@ -15,7 +15,11 @@ if stat>=0
     [mu_u, var_u]=pce_moments( u_alpha, I_u);
     std_u=sqrt(var_u);
     for i=0:stat
-        plot(x, mu_u*[1,1]+i*std_u*[-1,1], 'Color', col(1+round(i*size(col,1)/(stat+2)),:) );
+        plot_opts={'Color', col(1+round(i*size(col,1)/(stat+2)),:)};
+        if i==0
+            plot_opts={plot_opts{:}, 'LineWidth', 2};
+        end
+        plot(x, mu_u*[1,1]+i*std_u*[-1,1], plot_opts{:} );
         hold on;
     end
 end
