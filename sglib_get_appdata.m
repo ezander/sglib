575a45d7ed74459c87714ddef37d975034c5061a
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-07-22 14:21:30 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@183 ad3af975-698c-43d8-bc0c-eed00f6a1e86

---
+++ sglib_get_appdata.m
@@ -0,0 +1,34 @@
+function appdata=sglib_get_appdata( item, default )
+% SGLIB_GET_APPDATA Retrieves SGLib application specific data.
+%   SGLIB_GET_APPDATA retrieves a data structure in which SGLib methods
+%   store data about user settings, statistics, runtime information and the
+%   like.
+%
+% Example (<a href="matlab:run_example sglib_get_appdata">run</a>)
+%   sglib_get_appdata( 'basepath' )
+%   sglib_get_appdata
+%
+% See also SGLIB_SET_APPDATA
+
+%   Elmar Zander
+%   Copyright 2006, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+appdata=getappdata( 0, 'sglib' );
+if nargin>=1 && ~isempty(item)
+    if isfield(appdata,item)
+        appdata=appdata.(item);
+    elseif nargin>=2
+        appdata=default;
+    else
+        appdata=[];
+    end
+end
---
+++ sglib_set_appdata.m
@@ -0,0 +1,32 @@
+function sglib_set_appdata( appdata, item )
+% SGLIB_SET_APPDATA Stores SGLib application specific data.
+%   SGLIB_SET_APPDATA( APPDATA ) stores the complete SGLib app data
+%   structure. If ITEM is specified the data is stored in a structure field
+%   with name ITEM in the SGLib appdata.
+%
+% Example (<a href="matlab:run_example sglib_set_appdata">run</a>)
+%   stats=sglib_get_appdata( 'mystats', 0 )
+%   stats=stats+1;
+%   sglib_set_appdata( stats, 'mystats' );
+%   stats=sglib_get_appdata( 'mystats' )
+%
+% See also SGLIB_GET_APPDATA
+
+%   Elmar Zander
+%   Copyright 2006, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+if nargin>=2 && ~isempty(item)
+    data=appdata;
+    appdata=sglib_get_appdata();
+    appdata.(item)=data;
+end
+setappdata( 0, 'sglib', appdata );
