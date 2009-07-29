8a160617412a5538753616bc4bd83c7f5d19643a
ezander <ezander@ad3af975-698c-43d8-bc0c-eed00f6a1e86>
2009-07-29 10:15:27 +0000

    git-svn-id: https://valkyrie.sc.cs.tu-bs.de/svn/sglib/trunk/scripts@198 ad3af975-698c-43d8-bc0c-eed00f6a1e86

--- util/get_mfile_path.m
+++ util/get_mfile_path.m
@@ -1,4 +1,31 @@
 function path=get_mfile_path
+% GET_MFILE_PATH Return the complete path to the calling m-file.
+%   GET_MFILE_PATH Returns the path to the calling m-file. I.e. if called
+%   from '/home/userxyz/matlab/foo/bar.m' it will return
+%   '/home/userxyz/matlab/foo'.
+% 
+% Example: (<a href="matlab:run_example get_mfile_path">run</a>)
+%     % when run with 'run_example' it should  return the path 
+%     % of the util directory
+%     get_mfile_path
+% 
+% See also MFILENAME, FILEPARTS, DBSTACK
+
+%   Elmar Zander
+%   Copyright 2009, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
 
 s=dbstack('-completenames');
-path=fileparts(s(2).file);
+if length(s)<2
+    path='';
+else
+    path=fileparts(s(2).file);
+end
