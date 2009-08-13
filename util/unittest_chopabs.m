5c88bbdc81f1c4654d160e752107c771e8bfa7c4
Elmar Zander <e.zander@tu-bs.de>
2009-08-13 17:45:55 +0200

    chopabs function created

---
+++ util/unittest_chopabs.m
@@ -0,0 +1,27 @@
+function unittest_chopabs
+% UNITTEST_CHOPABS Test the CHOPABS function.
+%
+% Example (<a href="matlab:run_example unittest_chopabs">run</a>) 
+%    unittest_chopabs
+%
+% See also TESTSUITE, CHOPABS
+
+%   Elmar Zander
+%   Copyright 2009, Institute of Scientific Computing, TU Braunschweig.
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+assert_set_function( 'chopabs' );
+
+A=[ 1 2 1e-11 1e-3 3];
+assert_equals( chopabs(A), [1, 2, 0, 1e-3, 3], 'oneparam' );
+assert_equals( chopabs(A,1e-2), [1, 2, 0, 0, 3], 'twoparam1' );
+assert_equals( chopabs(A,1e-12), A, 'twoparam2' );
