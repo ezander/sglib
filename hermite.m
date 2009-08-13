e4a0104b1017254191c35549bf2a98d11477b1e3
Elmar Zander <ezander@tigerente.(none)>
2009-08-13 22:54:07 +0200

    Fixed up some comments.

--- hermite.m
+++ hermite.m
@@ -1,20 +1,20 @@
 function h=hermite( n, all_polys )
-%HERMITE  Return the n-th Hermite polynomial.
-%  H=HERMITE(N) with n integer and non-negative returns the n-th
-%  (probabilistic) Hermite polynomial H_i(x). The returned polynomials is
-%  not normalized so that <H_i H_j>=i! delta_ij, where <.> denotes the
-%  expected value with respect to the Gauss measure exp(-x^2/2)/sqrt(2*pi).
-%  The algoritm uses the three term recurrence
-%     $$H_{n+1}(x) = x H_n - n H_{n-1}$$
-%  with H_0=1 and H_1=x. The returned polynomial uses the standard matlab
-%  representation of polynomials where the first element corresponds to the
-%  highest power of x and the last element to the constant. E.g.
-%  hermite(4) returns [1,0,-6,0,3] which means x^4-6*x^2+3. Polynomials of
-%  this kind can be easily manipulated and evaluated in Matlab using the
-%  functions polyint (integration), polyder (derivatives), conv
-%  (multiplication) and polyval (evaluation).
+% HERMITE  Return the n-th Hermite polynomial.
+%   H=HERMITE(N) with n integer and non-negative returns the n-th
+%   (probabilistic) Hermite polynomial H_i(x). The returned polynomials is
+%   not normalized so that <H_i H_j>=i! delta_ij, where <.> denotes the
+%   expected value with respect to the Gauss measure exp(-x^2/2)/sqrt(2*pi).
+%   The algoritm uses the three term recurrence
+%      $$H_{n+1}(x) = x H_n - n H_{n-1}$$
+%   with H_0=1 and H_1=x. The returned polynomial uses the standard matlab
+%   representation of polynomials where the first element corresponds to the
+%   highest power of x and the last element to the constant. E.g.
+%   hermite(4) returns [1,0,-6,0,3] which means x^4-6*x^2+3. Polynomials of
+%   this kind can be easily manipulated and evaluated in Matlab using the
+%   functions polyint (integration), polyder (derivatives), conv
+%   (multiplication) and polyval (evaluation).
 %
-%  H=HERMITE(N,TRUE) returns all Hermite polynomials up to degree N.
+%   H=HERMITE(N,TRUE) returns all Hermite polynomials up to degree N.
 %
 % Example (<a href="matlab:run_example hermite">run</a>)
 %   h1=hermite( 1 ); format_poly( h1 );
