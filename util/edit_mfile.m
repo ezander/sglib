5cc4d2f48ebba39f2582772b7ac624849f3a7141
Elmar Zander <e.zander@tu-bs.de>
2009-08-17 12:31:20 +0200

    Improved edit_mfile to provide some predefined template.

--- util/edit_mfile.m
+++ util/edit_mfile.m
@@ -1,4 +1,4 @@
-function edit_mfile( filename )
+function edit_mfile( filename, type )
 %EDIT_MFILE Edit a new or existing m-file in the editor with path.
 %   EDIT_MFILE( FILENAME ) opens the m-file named by FILENAME in the Matlab
 %   internal editor. If FILENAME doesn't end in '.m' it is appended. This
@@ -24,8 +24,44 @@ function edit_mfile( filename )
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+author='Elmar Zander';
+institution='Institute of Scientific Computing, TU Braunschweig';
+
+d=date;
+year=d(end-3:end);
+
+
 if ~strcmp( filename(end-1:end), '.m' )
     filename=[filename '.m'];
 end
+[pathstr,name,ext] = fileparts(filename);
 
-com.mathworks.mlservices.MLEditorServices.openDocument( filename );
+if true || ~exist( filename, 'file' )
+    fid=fopen( filename, 'w' );
+    fprintf( fid, 'function ret=%s(varargin)\n', name );
+    fprintf( fid, '%%%s Short description of %s.\n', upper(name), name );
+    fprintf( fid, '%%   %s Long description of %s.\n', upper(name), name );
+%     fprintf( fid, '%%Options:\n' );
+%     fprintf( fid, '%%\n' );
+%     fprintf( fid, '%%Notes:\n' );
+    fprintf( fid, '%%\n' );
+    fprintf( fid, '%%Example (<a href="matlab:run_example %s">run</a>)\n', name );
+    fprintf( fid, '%%\n' );
+    fprintf( fid, '%%See also\n' );
+    fprintf( fid, '\n' );
+    fprintf( fid, '%%   %s\n', author );
+    fprintf( fid, '%%   Copyright %s, %s.\n', year, institution );
+    fprintf( fid, '%%   $Id$ \n' );
+    fprintf( fid, '%%\n' );
+    fprintf( fid, '%%   This program is free software: you can redistribute it and/or modify it\n' );
+    fprintf( fid, '%%   under the terms of the GNU General Public License as published by the\n' );
+    fprintf( fid, '%%   Free Software Foundation, either version 3 of the License, or (at your\n' );
+    fprintf( fid, '%%   option) any later version. \n' );
+    fprintf( fid, '%%   See the GNU General Public License for more details. You should have\n' );
+    fprintf( fid, '%%   received a copy of the GNU General Public License along with this\n' );
+    fprintf( fid, '%%   program.  If not, see <http://www.gnu.org/licenses/>.\n' );
+    fclose( fid );
+end
+
+edit( filename )
+%com.mathworks.mlservices.MLEditorServices.openDocument( filename );
