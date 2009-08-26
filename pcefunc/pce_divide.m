48eb87f85b80ea93b221896fa29db939fbf297eb
Elmar Zander <e.zander@tu-bs.de>
2009-08-26 15:43:35 +0200

    pce_divide: changed order of arguments; added unit test

--- pcefunc/pce_divide.m
+++ pcefunc/pce_divide.m
@@ -1,19 +1,19 @@
 function x_gamma=pce_divide( a_alpha, I_a, b_beta, I_b, I_x )
 
 if nargin<5
-    I_x=I_b;
+    I_x=I_a;
 end
 
-m_b=size(I_b,1);
+m_a=size(I_a,1);
 m_x=size(I_x,1);
-B=multiindex_factorial(I_b).*b_beta';
-K=zeros(m_b,m_x);
+A=compute_pce_rhs( a_alpha, I_a );
+K=zeros(m_a,m_x);
 
-for j=1:size(I_b,1)
-    beta=I_b(j,:);
-    for k=1:size(I_x,1);
+for j=1:m_a
+    alpha=I_a(j,:);
+    for k=1:m_x;
         gamma=I_x(k,:);
-        K(j,k)=a_alpha*squeeze(hermite_triple_fast(beta,gamma,I_a));
+        K(j,k)=b_beta*squeeze(hermite_triple_fast(alpha,gamma,I_b));
     end
 end
-x_gamma=(K\B)';
+x_gamma=(K\A')';
--- pcefunc/unittest_pce_divide.m
+++ pcefunc/unittest_pce_divide.m
@@ -4,7 +4,7 @@ function unittest_pce_divide
 % Example (<a href="matlab:run_example unittest_pce_divide">run</a>) 
 %    unittest_pce_divide
 %
-% See also TESTSUITE
+% See also PCE_DIVIDE, TESTSUITE
 
 %   Elmar Zander
 %   Copyright 2007, Institute of Scientific Computing, TU Braunschweig.
@@ -21,13 +21,15 @@ function unittest_pce_divide
 
 assert_set_function( 'pce_divide' );
 
-p=10;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% test 1: single rand. var, using anon functions
 
 % Define the random variables A, B and X.
 %a_func={@exponential_stdnor, {2}};
-a_func={@uniform_stdnor, {2,3}};
-b_func={@lognormal_stdnor, {1.5, 0.5}};
-x_func={@div_func,{b_func, a_func}};
+p=10;
+a_func={@lognormal_stdnor, {1.5, 0.5}};
+b_func={@uniform_stdnor, {2,3}};
+x_func={@div_func,{a_func, b_func}};
 
 % method 3: pce by b_func/a_func
 x_gamma_ex=pce_expand_1d( x_func, p );
@@ -36,21 +38,62 @@ x_gamma_ex=pce_expand_1d( x_func, p );
 [a_alpha,I_a]=pce_expand_1d( a_func, p );
 [b_beta,I_b]=pce_expand_1d( b_func, p );
 
-tic
-for i=1:1+00
-    x_gamma=pce_divide( a_alpha, I_a, b_beta, I_b );
-end
-%toc
+% direct check
+x_gamma=pce_divide( a_alpha, I_a, b_beta, I_b );
+assert_equals( x_gamma(1:5), x_gamma_ex(1:5), 'pce_coeff', 'reltol', 10.^-[7,5,4,4,2] );
 
-if 0
-    disp( [x_gamma_ex; x_gamma]' );
-    disp( (x_gamma_ex-x_gamma)'./x_gamma_ex'*100 );
-end
+% realization check (this is not stochastic like MC or something, should
+% hold for any realization).
 
-assert_equals( x_gamma(1:5), x_gamma_ex(1:5), 'pce_coeff', 'reltol', 0.1.^[7,5,4,4,2] );
+xi=randn_sorted(10);
+A=pce_field_realization( 1, a_alpha, I_a, xi );
+B=pce_field_realization( 1, b_beta, I_b, xi );
+X=pce_field_realization( 1, x_gamma, I_b, xi );
+assert_equals( X, A./B, 'vec_rand_multi', 'reltol', 0.001 );
+return
+
+%%% test 2: just do it with the previous pce variables doubled
+x_gamma_ex=[x_gamma_ex;x_gamma_ex];
+x_gamma=pce_divide( [a_alpha;a_alpha], I_a, [b_beta;b_beta], I_b );
+assert_equals( x_gamma(1:5), x_gamma_ex(1:5), 'pce_coeff', 'reltol', 10.^-[7,5,4,4,2] );
+return
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% test for multiplication of a two random vector of univariate Hermites
+% equality checked with random numbers, not directly on the polynomials
+N=10;
+X_alpha=rand(N,5); I_X=(0:4)';
+Y_beta=rand(N,7);  I_Y=(0:6)';
+[Z_gamma,I_Z]=pce_multiply( X_alpha, I_X, Y_beta, I_Y );
+
+[Z,xi]=pce_field_realization( 1, Z_gamma, I_Z );
+[X]=pce_field_realization( 1, X_alpha, I_X, xi );
+[Y]=pce_field_realization( 1, Y_beta, I_Y, xi );
+assert_equals( Z, X.*Y, 'vec_rand' );
+
+% test for multiplication of a two random vector of multivariate Hermites
+% equality checked with random numbers, not directly on the polynomials
+N=10; m=3; p_X=2; p_Y=4;
+I_X=multiindex(m,p_X); X_alpha=rand(N,size(I_X,1)); 
+I_Y=multiindex(m,p_Y); Y_beta=rand(N,size(I_Y,1)); 
+[Z_gamma,I_Z]=pce_multiply( X_alpha, I_X, Y_beta, I_Y );
+
+[Z,xi]=pce_field_realization( 1, Z_gamma, I_Z );
+[X]=pce_field_realization( 1, X_alpha, I_X, xi );
+[Y]=pce_field_realization( 1, Y_beta, I_Y, xi );
+assert_equals( Z, X.*Y, 'vec_rand_multi' );
 
 
 
 function y=div_func( x, num_func, den_func )
 y=funcall(num_func,x)./funcall(den_func,x);
+
+
 
