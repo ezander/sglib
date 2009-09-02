5983493ffb9b3b162e6c5946fa409ef6a86a3c6b
Elmar Zander <e.zander@tu-bs.de>
2009-09-02 16:34:49 +0200

    pce_to_kl: updated comments; unified output behaviour for spat. eigenfuncs; wrote unit test;

--- unittest_pce_to_kl.m
+++ unittest_pce_to_kl.m
@@ -1,4 +1,4 @@
-function unittest_pce_to_kl
+%function unittest_pce_to_kl
 % UNITTEST_PCE_TO_KL Test the PCE_TO_KL function.
 %
 % Example (<a href="matlab:run_example unittest_pce_to_kl">run</a>)
@@ -7,7 +7,7 @@ function unittest_pce_to_kl
 % See also TESTSUITE
 
 %   Elmar Zander
-%   Copyright 2007, Institute of Scientific Computing, TU Braunschweig.
+%   Copyright 2009, Institute of Scientific Computing, TU Braunschweig.
 %   $Id$
 %
 %   This program is free software: you can redistribute it and/or modify it
@@ -19,3 +19,33 @@ function unittest_pce_to_kl
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+% we don't check directly the result here, but rather some properties
+% 1. mean of KL is mean of PCE
+% 2. spatial eigenfunctions are orthogonal with norm equal to KL
+%    eigenvalues
+% 3. stochastic eigenfunction (random vars) are orthogonal
+% 4. we get the PCE back from the KL
+
+N=51;
+[els,pos]=create_mesh_1d( N, 0, 1 );
+G_N=mass_matrix( els, pos );
+p_kap=3;
+m_kap=3;
+M_kap=nchoosek( p_kap+m_kap, m_kap ); % full kl without truncation
+l_kap=M_kap; % full kl without truncation
+lc_kap=0.3;
+stdnor_kap={@beta_stdnor,{4,2}};
+cov_kap={@gaussian_covariance,{lc_kap,1}};
+[kap_i_alpha, I_kap]=expand_field_pce_sg( stdnor_kap, cov_kap, [], pos, G_N, p_kap, m_kap );
+[kap_i_0,kap_i_k,kap_k_alpha,relerr,sigma_k]=pce_to_kl( kap_i_alpha, I_kap, l_kap, G_N );
+
+
+G_Phi=diag(multiindex_factorial(I_kap));
+
+assert_equals( kap_i_0, kap_i_alpha(:,1), 'mean' );
+assert_equals( kap_i_k'*G_N*kap_i_k, diag(sigma_k)^2, 'spat_orth' );
+assert_equals( kap_k_alpha*G_Phi*kap_k_alpha', eye(M_kap), 'stoch_orth' );
+
+kap2_i_alpha=kap_i_k*kap_k_alpha;
+kap2_i_alpha(:,1)=kap_i_0;
+assert_equals( kap2_i_alpha, kap_i_alpha, 'pce_back' );
