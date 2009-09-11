8e83897fe7f5dee679991866bf8d1dcf1d21b28d
Elmar Zander <e.zander@tu-bs.de>
2009-09-11 13:46:48 +0200

    demo_animation: changed to spline interpolation for smoother animation

--- demo/demo_animation.m
+++ demo/demo_animation.m
@@ -2,38 +2,34 @@ function demo_animation
 % DEMO_ANIMATION Shows a smoothly varying random field.
 
 clf;
-
 if ismatlab
     set(gcf, 'WindowButtonUpFcn', @stop_demo );
 else
     error( 'This demo does not work with Octave yet.' );
 end
 
-%[p_f, m_gam_f, m_f, lc_f, h_f, cov_f, cov_gam, els, pos, f_alpha, I_f, mu_f, f_i_alpha, v_f]=
+
 kl_model_version=[];
 [m_gam_f, els, pos, mu_f, v_f, f_i_alpha, I_f]=...
     load_kl_model( 'rf_kl_2d-1', kl_model_version, [], {'m_gam_r', 'els', 'pos', 'mu_r_j', 'r_j_i', 'rho_i_alpha', 'I_r'} );
 
+N=200;
+xi_N=randn(N,m_gam_f);
+pp = interp1(linspace(1,1000,N),xi_N,'spline','pp');
 
-xi1=randn(1,m_gam_f);
-xi2=randn(1,m_gam_f);
 set( gcf, 'UserData', [] ); % just in case
 disp('Click into the graphics window to stop the demo...' );
-for i=1:100
-    for s=0:0.1:0.9
-        sp=s^1.5;
-        xi=(1-sp)*xi1+sp*xi2;
-        f_ex=kl_pce_field_realization( pos, mu_f, v_f, f_i_alpha, I_f, xi );
-        plot_field( els, pos, f_ex );
+for i=1:1000
+    xi=ppval(pp,i)';
+    xi=(xi-mean(xi))/sqrt(var(xi))+mean(xi);
+    f_ex=kl_pce_field_realization( pos, mu_f, v_f, f_i_alpha, I_f, xi );
+    plot_field( els, pos, f_ex, 'lighting', 'gouraud' );
 
-        if ~isempty( get( gcf, 'UserData' ) )
-            set( gcf, 'UserData', [] );
-            disp('quitting...');
-            return
-        end
+    if ~isempty( get( gcf, 'UserData' ) )
+        set( gcf, 'UserData', [] );
+        disp('quitting...');
+        return
     end
-    xi1=xi2;
-    xi2=randn(1,m_gam_f);
 end
 
 
