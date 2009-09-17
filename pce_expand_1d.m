26f8abae6a2898e7da379f99115e69fbca975b02
Elmar Zander <e.zander@tu-bs.de>
2009-09-17 15:41:33 +0200

    pce_expand_1d: renaming of parameters

--- pce_expand_1d.m
+++ pce_expand_1d.m
@@ -1,30 +1,29 @@
-function [pce_coeff,pce_ind]=pce_expand_1d( func, p )
-% PCE_EXPAND_1D Calculate the PCE expansion in one stochastics dimension.
-%   [PCE_COEFF,PCE_IND,POLY_COEFF]=PCE_EXPAND_1D( F, P ) gives the polynomial
-%   chaos expansion of a random variable X with X=f(gamma), i.e. the
-%   random variable must be a transformed gaussian random variable. For
-%   example for a lognormal random variable f=exp. This is possible for
-%   an arbitrary random variable by specifying f(x)=F^-1(Phi(x)) where  F
-%   and Phi are the cumulative density function of the expanded random
-%   variable and the gaussian rv respectively. p is the order of the
-%   expansion, thus the returned arrrays have dimension p+1. In PCE_COEFF
-%   are the coefficients of the Hermite polynomials in the expansion such
-%   that HERM_COEFF(i) is the coefficient of H_{PCE_IND(i)}. In POLY_COEFF are the
-%   coefficients of the fully expanded polynomial in Matlab standard order
-%   such that poly_coeff(p+1-i) is the coefficient of x^i. The polynomials
-%   can most easily be evaluted at some point x by polyval(poly_coeff,x).
+function [X_alpha,I]=pce_expand_1d( func, p )
+% PCE_EXPAND_1D Calculate the PC expansion in one stochastics dimension.
+%   [X_ALPHA,I]=PCE_EXPAND_1D( FUNC, P ) gives the polynomial chaos
+%   expansion of a random variable X with X=FUNC(gamma), i.e. the random
+%   variable must be a transformed Gaussian random variable. E.g. for a
+%   lognormal random variable you would pass FUNC=@EXP (for some common
+%   distributions those functions are given by XXX_STDNOR where XXX is the
+%   name of the distribution). This is possible for an arbitrary random
+%   variable by specifying F(x)=F^-1(Phi(x)) where  F and Phi are the
+%   cumulative density function of the expanded random variable and the
+%   Gaussian RV respectively. P is the order of the expansion, thus the
+%   returned arrrays have dimension P+1. In X_ALPHA are the coefficients of
+%   the Hermite polynomials in the expansion such that X_ALPHA(i) is the
+%   coefficient of H_{I(i)}.
 %
 % Note:
-%   PCE_IND is somehow unnecessary for a univariate PC expansion, it's
+%   I is somehow unnecessary for a univariate PC expansion, it's
 %   just there to make it more compatible with the multivariate PC
 %   expansions, and to let you easily feed in results from this function
 %   into functions that take multivariate PCEs.
 %
 % Example (<a href="matlab:run_example pce_expand_1d">run</a>)
-%   [pcc,pci,poc]=pce_expand_1d( @exp, 4 );
-%   x=linspace(-1,1); y=hermite_val(pcc,x);
+%   [Log_alpha,I]=pce_expand_1d( {@lognormal_stdnor, {0, 1}, {2, 3}}, 4 );
+%   x=linspace(-1,1); y=hermite_val(Log_alpha,x);
 %   plot(x,y);
-%   disp(pcc);
+%   disp(Log_alpha);
 %   disp(exp(1/2)./factorial(0:4)); % should be the same
 %
 % See also PCE_EXPAND_1D_MC
@@ -47,16 +46,16 @@ if nargin==0
     return
 end
 
-pce_coeff=zeros(1,p+1);
-pce_ind=(0:p)';
+X_alpha=zeros(1,p+1);
+I=(0:p)';
+order=2*max(p,1)+2;
 
 for i=0:p
     h=hermite(i);
     int_func={@int_kernel, {func, h}, {1, 2}};
-    order=2*max(p,1)+2;
-    pce_coeff(i+1)=gauss_hermite(int_func,order)/factorial(i);
+    X_alpha(i+1)=gauss_hermite(int_func,order)/factorial(i);
 end
 
 function y=int_kernel(int_func, h, x)
-% INT_KERNEL evaluate the integral kernel for gauss hermite integration.
+% INT_KERNEL evaluate the pce integral kernel for gauss hermite integration.
 y=funcall( int_func, x).*polyval(h,x);
