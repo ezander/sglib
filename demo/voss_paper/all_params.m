f750d470c2d47c0a18ce01f577e9898310fa1207
Elmar Zander <e.zander@tu-bs.de>
2009-11-10 15:04:45 +0100

    Some changes for voss paper.

--- demo/voss_paper/all_params.m
+++ demo/voss_paper/all_params.m
@@ -1,9 +1,22 @@
+% check size, accuracy
+% check contractivity
+
 clear
 clc
 if ~exist( './mat', 'dir' )
     mkdir( 'mat' );
 end
 n_max=0;
+
+
+% params_defs={
+%   'beta_a', [0.25, 1, 4], 'beta_1b$log2(beta_a)$';
+%   'solver', {'cg', 'si'}, 'solver_$solver$';
+%   'trunc_mode', {'euc', 'klm'}, 'trm_$trunc_mode$';
+%   'eps_mode', {'eps', 'var'}, '$eps_mode$';
+%   'eps', 10.^-[4,6,8], '1e$log10(eps)$'
+% }
+
 
 for dryrun=[true,false]
     n=0;
@@ -22,10 +35,10 @@ for dryrun=[true,false]
                     solver=solvers{solver_num};
                     solver_desc=solver;
 
-                    for eps=10.^-[4,6,8]
+                    for eps=10.^-[4]%,6,8]
                         eps_desc=sprintf('1e%d', log10(eps) );
 
-                        for eps_var=[false, true]
+                        for eps_var=[false]%, true]
                             eps_var_modes={'eps','var'};
                             eps_var_desc=eps_var_modes{eps_var+1};
 
@@ -33,18 +46,13 @@ for dryrun=[true,false]
                             solver_base=sprintf( '%s-%s_%s-%s_%s', solver_desc, trunc_mode_desc, trunc_klmode_desc, eps_var_desc, eps_desc );
                             res_base=sprintf( '%s-%s', model_base, solver_base );
                             
-                            model_base2=strvarexpand( 'beta_1b$log2(beta_a)$');
-                            solver_base2=strvarexpand( '$solver$-$trunc_klmodes{trunc_klmode+1}$_trm_$trunc_mode$-$eps_var_modes{eps_var+1}$_1e$log10(eps)$' );
-                            res_base2=sprintf( '%s-%s', model_base2, solver_base2 );
+%                             model_base2=strvarexpand( 'beta_1b$log2(beta_a)$');
+%                             solver_base2=strvarexpand( '$solver$-trm_$trunc_mode$_$trunc_klmodes{trunc_klmode+1}$-$eps_var_modes{eps_var+1}$_1e$log10(eps)$' );
+%                             res_base2=sprintf( '%s-%s', model_base2, solver_base2 );
                             
                             n=n+1;
                             if dryrun; continue; end
-                            %solver_base=sprintf('%s-%s_1e%d-tm%d', solver{1}, eps_mode{eps(2)+1}, log10(eps(1)), trunc_mode(1));
-                            %disp({eps(1),eps(2),, trunc_mode(2), delta,})
-                            fprintf( '%3d/%3d - %s\n', n, n_max, res_base );
-                            fprintf( '%3d/%3d - %s\n', n, n_max, res_base2 );
-                            % check size, accuracy
-                            % check contractivity
+                            fprintf( '%03d/%03d - %s\n', n, n_max, res_base );
                             
                         	model_param_file=['./mat/model_params-' model_base '.mat'];
                             if ~exist(model_param_file, 'file')
--- demo/voss_paper/build_model.m
+++ demo/voss_paper/build_model.m
@@ -1,11 +1,23 @@
 function build_model( pre_run, post_run )
 
+% ==========
+% = prolog =
+% ==========
 eval( pre_run );
-
-
 disp('building model')
-whos
-keyboard
+%whos
+disp(strvarexpand( 'beta_a=$beta_a$'))
+%keyboard
+
+% ==========
+% =  main  =
+% ==========
 
 
+
+
+
+% ==========
+% = epilog =
+% ==========
 eval( post_run );
