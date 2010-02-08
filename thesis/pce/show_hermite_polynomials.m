3d467b1ebca26a71b5f85e84e53a31691a28e5bb
Elmar Zander <e.zander@tu-bs.de>
2010-02-08 12:25:54 +0100

    added show_hermite_poly stuff

--- thesis/pce/show_hermite_polynomials.m
+++ thesis/pce/show_hermite_polynomials.m
@@ -1,19 +1,27 @@
 function show_hermite_polynomials
 
 x=linspace(-3.5,3.5);
-y=hermite_val( unitvector(5)', x );
-y=y.*exp(-x.^2/2)/sqrt(2*pi);
+y=pce_evaluate( eye(5), multiindex(1,4), x )
+y=row_col_mult( y, exp(-x.^2/2)/sqrt(2*pi) );
 plot(x,y)
+xlabel('x');
+ylabel('y');
+legend('h_0','h_1','h_2','h_3','h_4')
+title('Hermite functions')
+grid on
+save_thesis_figure( 'hermite_functions_1d', {} );
 
-
-x=linspace(-3.5,3.5);
-y=hermite_val_multi( unitvector([1,3,5])', x );
-y=y.*exp(-x.^2/2)/sqrt(2*pi);
+x=linspace(-3,3);
+y=pce_evaluate( eye(5), multiindex(1,4), x )
+%y=row_col_mult( y, exp(-x.^2/2)/sqrt(2*pi) );
 plot(x,y)
+xlabel('x');
+ylabel('y');
+legend('H_0','H_1','H_2','H_3','H_4')
+ylim([-10,10])
+grid on
+title('Hermite polynomials')
+save_thesis_figure( 'hermite_polynomials_1d', {} );
 
-p=hermite(4, true);
-x=linspace(-3.5,3.5);
-y=polyval(p,x);
-%y=y.*exp(-x.^2/2)/sqrt(2*pi);
 
-plot(x,y)
+
