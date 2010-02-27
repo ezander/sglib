437a4f85c05d4e3ee2f3b08b5e54a77d64a49c2b
Elmar Zander <e.zander@tu-bs.de>
2010-02-27 17:56:01 +0100

    WIP: added expand_field_kl_pce

--- expand_field_kl_pce.m
+++ expand_field_kl_pce.m
@@ -1,4 +1,14 @@
-function [mu_r_i,r_i_k,r_k_alpha,I_r]=expand_field_kl_pce( rho_stdnor_func, cov_r_func, cov_gam_func, pos, G_N, p, m_gam, l_r, varargin )
+function [r_i_k,r_k_alpha,I_r]=expand_field_kl_pce( r_stdnor_func, cov_r_func,  pos, G_N, p_r, m_r, l_r, varargin )
 
-[r_i_alpha, I_r]=expand_field_pce_sg( rho_stdnor_func, cov_r_func, cov_gam_func, pos, G_N, p, m_gam, varargin{:} );
-[mu_r_i,r_i_k,r_k_alpha]=pce_to_kl( r_i_alpha, I_r, l_r, G_N );
+% cov_gam_func, from options
+
+[r_i_alpha, I_r]=expand_field_pce_sg( r_stdnor_func, cov_r_func, cov_gam_func, pos, G_N, p_r, m_r );
+
+if true
+    C=covariance_matrix( pos, cov_r_func );
+    v_r_i=kl_solve_evp( C, G_N, l_r );
+    [r_i_k,r_k_alpha]=project_pce_on_kl( r_i_alpha, I_r, v_r_i );
+else
+    [r_i_k,r_k_alpha]=pce_to_kl( r_i_alpha, I_r, l_r, G_N );
+end
+
---
+++ thesis/solver/get_param.m
@@ -0,0 +1,21 @@
+function value=get_param(name, default)
+% GET_PARAM Short description of get_param.
+%   GET_PARAM Long description of get_param.
+%
+% Example (<a href="matlab:run_example get_param">run</a>)
+%
+% See also
+
+%   Elmar Zander
+%   Copyright 2010, Inst. of Scientific Computing, TU Braunschweig
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+value=default;
