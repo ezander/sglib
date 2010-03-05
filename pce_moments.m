fb92798b732f898fbd75e0efdd419401b77e3342
Elmar Zander <e.zander@tu-bs.de>
2010-03-05 20:01:55 +0100

    WIP: overhaul of pce functions

--- pce_moments.m
+++ pce_moments.m
@@ -1,19 +1,19 @@
-function [mean,var,skew,kurt]=pce_moments( pcc, pci ) %#ok: kurt never assigned
+function [mean,var,skew,kurt]=pce_moments( r_i_alpha, I_r ) %#ok: kurt never assigned
 % PCE_MOMENTS Calculate the statistical moments of a distribution given as PCE.
-%   [MEAN,VAR,SKEW,KURT]=PCE_MOMENTS( PCC, PCI ) calculate mean, variance,
+%   [MEAN,VAR,SKEW,KURT]=PCE_MOMENTS( R_I_ALPHA, I_R ) calculate mean, variance,
 %   skewness and kurtosis for a distribution given by the coefficients in
-%   PCC. PCC can also be a field of PC expansions where PCC(i,:) is the
+%   R_I_ALPHA. R_I_ALPHA can also be a field of PC expansions where R_I_ALPHA(i,:) is the
 %   expansion at point x_i. The output arguments VAR, SKEW and KURT are
-%   optional and only calculated if required. PCI is optional and contains
+%   optional and only calculated if required. I_R is optional and contains
 %   the indices of the Hermite polynomials.
 %
 %   Caveat: currently only MEAN, VAR and SKEW are calculated. KURT has
 %   to be implemented yet.
 %
 % Example (<a href="matlab:run_example pce_moments">run</a>)
-%   [pcc,pci]=pce_expand_1d( @exp, 12 );
-%   [mean,var,skew]=pce_moments( pcc );
-%   [mean,var,skew]=pce_moments( pcc, pci );
+%   [r_i_alpha,I_r]=pce_expand_1d( @exp, 12 );
+%   [mean,var,skew]=pce_moments( r_i_alpha );
+%   [mean,var,skew]=pce_moments( r_i_alpha, I_r );
 %   [mean,var,skew]=lognormal_moments( 0, 1);
 %
 % See also PCE_EXPAND_1D, DATA_MOMENTS
@@ -32,11 +32,11 @@ function [mean,var,skew,kurt]=pce_moments( pcc, pci ) %#ok: kurt never assigned
 
 
 if nargin==1
-    p=size(pcc,2)-1;
-    n=size(pcc,1);
+    p=size(r_i_alpha,2)-1;
+    n=size(r_i_alpha,1);
 
-    % Coefficient a_0 (=pcc(1)) of H_0 is the mean of the PCE
-    mean = pcc(:,1);
+    % Coefficient a_0 (=r_i_alpha(1)) of H_0 is the mean of the PCE
+    mean = r_i_alpha(:,1);
 
     % Variance is equal to \sum_{i=1}^\infty i\! a_i^2
     % (Important: start with 1(2) to exclude mean).
@@ -44,7 +44,7 @@ if nargin==1
     % coefficients, while the seconds part calculates the factorials and
     % repeats them for all entries.)
     if nargout>=2
-        var = sum(pcc(:,2:end).^2.*repmat(factorial(1:p),n,1),2);
+        var = sum(r_i_alpha(:,2:end).^2.*repmat(factorial(1:p),n,1),2);
     end
     if nargout>=3
         skew=zeros(n,1);
@@ -65,7 +65,7 @@ if nargin==1
                         else % i==j && j==k
                             n_perm=1;
                         end
-                        skew=skew+pcc(:,i+1).*pcc(:,j+1).*pcc(:,k+1)*h_ijk*n_perm;
+                        skew=skew+r_i_alpha(:,i+1).*r_i_alpha(:,j+1).*r_i_alpha(:,k+1)*h_ijk*n_perm;
                     end
                 end
             end
@@ -74,24 +74,24 @@ if nargin==1
     end
 else
     % get the max. order of the pce
-    p=size(pcc,2)-1;
+    p=size(r_i_alpha,2)-1;
     % get the number of coefficient sets (i.e. the number of multivariate
     % Hermite polynomials)
-    n=size(pcc,1);
+    n=size(r_i_alpha,1);
 
-    % the first row in PCI should contain the mean (i.e. all indices have
+    % the first row in I_R should contain the mean (i.e. all indices have
     % to be zero)
-    if any(full(pci(1,:))~=0)
-        error('pce_moments: the first row in argument pci has to be identical zero!' );
+    if any(full(I_r(1,:))~=0)
+        error('pce_moments: the first row in argument I_r has to be identical zero!' );
     end
 
-    mean = pcc(:,1);
+    mean = r_i_alpha(:,1);
     if nargout>=2
         %         var=zeros(1,n);
         %         for i=1:n
-        %             var(i)=pcc(i,2:end).^2*multiindex_factorial(pci(2:end,:));
+        %             var(i)=r_i_alpha(i,2:end).^2*multiindex_factorial(I_r(2:end,:));
         %         end
-        var=pcc(:,2:end).^2*multiindex_factorial(pci(2:end,:));
+        var=r_i_alpha(:,2:end).^2*multiindex_factorial(I_r(2:end,:));
     end
     if nargout>=3
         %TODO: maybe for large multiindex sets the skewness should be
@@ -99,11 +99,11 @@ else
         % speed things up?)
 
         skew=zeros(n,1);
-        hermite_triple_fast(max(pci(:))); % initialize
+        hermite_triple_fast(max(I_r(:))); % initialize
         for i=2:p+1
             for j=2:i
                 for k=2:j
-                    h_abc=hermite_triple_fast(pci(i,:),pci(j,:),pci(k,:));
+                    h_abc=hermite_triple_fast(I_r(i,:),I_r(j,:),I_r(k,:));
                     if h_abc~=0
                         % correct for possible permutations (because j and
                         % k don't run over the full range but only up to
@@ -118,7 +118,7 @@ else
                             n_perm=1;
                         end
 
-                        skew=skew+pcc(:,i).*pcc(:,j).*pcc(:,k)*h_abc*n_perm;
+                        skew=skew+r_i_alpha(:,i).*r_i_alpha(:,j).*r_i_alpha(:,k)*h_abc*n_perm;
                     end
                 end
             end
