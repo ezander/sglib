1ba4e620ec33a3a62ac596d07913b5802e20dd3c
Elmar Zander <e.zander@tu-bs.de>
2010-05-12 16:57:07 +0200

    voss stuff added

---
+++ thesis/voss/show_solve_large_model_tensor.m
@@ -0,0 +1,26 @@
+
+rebuild=get_param('rebuild', false);
+%autoloader( {'model_1d_large'; 'define_geometry'; 'discretize_model'; 'setup_equation'; 'solve_by_pcg'; 'vector_to_tensor'}, rebuild, 'caller' );
+autoloader( {'model_1d_large'; 'define_geometry'; 'discretize_model'; 'setup_equation'; 'solve_by_gsolve_pcg' }, rebuild, 'caller' );
+rebuild=false;
+
+info_cg=info;
+%[rho,flag]=simple_iteration_contractivity( Ki, Mi_inv,  'abstol', 1e-2 )
+Ui_vec_true=Ui_vec;
+solve_by_gsolve_simple
+
+
+info_si=info;
+
+%%
+multiplot_init(1,1)
+plot(info_cg.resvec,'-x')
+plot(info_si.resvec,'-x')
+logaxis( 'y' )
+grid on
+
+%% Statitics
+% calling stoch. mat-vector prod
+% calling determ. mat-vector prod
+% calling determ. precond
+% calling reduction 
