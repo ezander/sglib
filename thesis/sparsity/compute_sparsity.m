d435192de11e11b0beb543c5777e36486775b24c
Elmar Zander <e.zander@tu-bs.de>
2010-06-07 14:29:26 +0200

    finished work on sparsity stuff

--- thesis/sparsity/compute_sparsity.m
+++ thesis/sparsity/compute_sparsity.m
@@ -4,9 +4,9 @@ I_k=multiindex( m_k, p_k );
 I_f=multiindex( m_f, p_f );
 
 if kfirst
-    [I_k,I_f,I_u]=multiindex_combine({I_k,I_f},p_u);
+    [I_k,I_f,I_u]=multiindex_combine({I_k,I_f},p_u); %#ok<ASGLU>
 else
-    [I_f,I_k,I_u]=multiindex_combine({I_f,I_k},p_u);
+    [I_f,I_k,I_u]=multiindex_combine({I_f,I_k},p_u); %#ok<ASGLU>
 end
 
 if alphasort
--- thesis/sparsity/show_sparsity_block_struct.m
+++ thesis/sparsity/show_sparsity_block_struct.m
@@ -3,7 +3,7 @@ function show_sparsity_block_struct
 p_u=3;
 m_k=5; p_k=6;
 m_f=5; p_f=3;
-alphasort=true
+alphasort=true;
 kfirst=false;
 
 multiplot_init(1,1);
@@ -14,10 +14,10 @@ multiplot;
 spy2(S);
 
 % show I_f block structure
-k_mask=any(I_k,1)
+k_mask=any(I_k,1);
 f_mask=~k_mask;
 x=(0:size(I_u,1)-1)+0.5;
-[b,m,n]=unique( I_u( :, f_mask ), 'rows' );
+[b,m,n]=unique( I_u( :, f_mask ), 'rows' ); %#ok<ASGLU>
 stairs( x, size(I_u,1)-n', 'r' )
 
 % show "sparse" sparsity pattern
