f49ea6745e97fee5b0864c88731223c262be806e
Elmar Zander <e.zander@tu-bs.de>
2010-10-15 13:19:20 +0200

    demo for solving the deterministic case added

--- plot/same_scaling.m
+++ plot/same_scaling.m
@@ -1,21 +1,22 @@
-function same_scaling( handles, axis )
+function same_scaling( handles, axes )
 
 if nargin<2
-    axis='z';
+    axes='z';
 end
 handles=handles(:);
 
 %%
-for i=1:length(handles)
-    z=get( handles(i), [axis 'lim'] );
-    if i==1
-        zl=z;
-    else
-        zl=[min([z zl]), max([z zl])];
+for axis=axes
+    for i=1:length(handles)
+        z=get( handles(i), [axis 'lim'] );
+        if i==1
+            zl=z;
+        else
+            zl=[min([z zl]), max([z zl])];
+        end
+    end
+    for i=1:length(handles)
+        set( handles(i), [axis 'lim'], zl );
     end
 end
-for i=1:length(handles)
-    set( handles(i), [axis 'lim'], zl );
-end
-
 
