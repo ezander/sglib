84757c95427f65024b745f6cf569ff446cb95e2a
Elmar Zander <elmar@zandere.de>
2011-02-18 20:26:47 +0100

    pce_error_est added

---
+++ pce_error_est.m
@@ -0,0 +1,48 @@
+function err=pce_error_est( x_i_alpha, I_x, y_i_alpha, I_y )
+% PCE_ERROR_EST Computes the L2-error between two PCE expansions.
+%   PCE_ERROR_EST Long description of pce_error_est.
+%
+% Example (<a href="matlab:run_example pce_error_est">run</a>)
+%
+% See also
+
+%   Elmar Zander
+%   Copyright 2011, Inst. of Scientific Computing, TU Braunschweig
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+% After the following few lines cindx and cindy will contain the indices in
+% I_x and I_y that are the same in both (i.e. I_x(cindx,:)==I_y(cindy,:))
+% and onlyx and onlyy will contain the indices that are only in I_x or I_y,
+% respectively. 
+
+if isequal(size(I_x),size(I_y)) && isequal(I_x, I_y)
+    cindx=(1:size(I_x,1));
+    cindy=cindx;
+    onlyx=[];
+    onlyy=[];
+else
+    if size(I_x,2)~=size(I_y,2)
+        error( 'sglib:pce_error_est:size_mismatch', 'multiindices are defined on a different number of random variables' )
+    end
+    Mx=size(I_x,1);
+    My=size(I_y,1);
+    [cindy,cindx]=multiindex_find( I_x, I_y );
+    onlyx=setdiff( (1:Mx)', cindx );
+    onlyy=setdiff( (1:My)', cindy );
+end
+
+
+d2c=(x_i_alpha(:,cindx)-y_i_alpha(:,cindy)).^2*multiindex_factorial(I_x(cindx,:) );
+d2x=x_i_alpha(:,onlyx).^2*multiindex_factorial(I_x(onlyx,:));
+d2y=y_i_alpha(:,onlyy).^2*multiindex_factorial(I_y(onlyy,:));
+err=sqrt(d2c+d2x+d2y);
---
+++ unittest_pce_error_est.m
@@ -0,0 +1,49 @@
+function unittest_pce_error_est
+% UNITTEST_PCE_ERROR_EST Test the PCE_ERROR_EST function.
+%
+% Example (<a href="matlab:run_example unittest_pce_error_est">run</a>)
+%   unittest_pce_error_est
+%
+% See also PCE_ERROR_EST, TESTSUITE 
+
+%   Elmar Zander
+%   Copyright 2011, Inst. of Scientific Computing, TU Braunschweig
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+munit_set_function( 'pce_error_est' );
+
+Ix=multiindex(4,2);
+Iy=multiindex(4,3);
+Mx=size(Ix,1);
+My=size(Iy,1);
+N=10;
+x_i_alpha=rand(N,Mx);
+d_i_alpha=rand(N,My-Mx);
+y_i_alpha=[x_i_alpha, d_i_alpha];
+[dummy,ex]=pce_moments( [zeros(N,Mx), d_i_alpha], Iy );
+ex=sqrt(ex);
+assert_equals( ex, pce_error_est( x_i_alpha, Ix, y_i_alpha, Iy ), 'xy' );
+assert_equals( ex, pce_error_est( y_i_alpha, Iy, x_i_alpha, Ix ), 'yx' );
+
+px=[1, randperm( Mx-1 )+1];
+py=[1, randperm( My-1 )+1];
+x_i_alpha=x_i_alpha(:,px);
+Ix=Ix(px,:);
+y_i_alpha=y_i_alpha(:,py);
+Iy=Iy(py,:);
+assert_equals( ex, pce_error_est( x_i_alpha, Ix, y_i_alpha, Iy ), 'pxpy' );
+
+
+
+
+
+
+
