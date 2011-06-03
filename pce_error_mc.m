4bb94e30e82b65afc2a9315f520941e0d8bfbaf1
Elmar Zander <e.zander@tu-bs.de>
2011-06-03 15:17:54 +0200

    pce_error_mc: added new function

---
+++ pce_error_mc.m
@@ -0,0 +1,49 @@
+function err=pce_error_mc(pce_func1, pce_func2, m, varargin)
+% PCE_ERROR_MC Evalutes the difference between two PCE reps .
+%   PCE_ERROR_MC Long description of pce_error_mc.
+%
+% Example (<a href="matlab:run_example pce_error_mc">run</a>)
+%
+% See also
+
+%   Elmar Zander
+%   Copyright 2011, Inst. of Scientific Comuting
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+options=varargin2options(varargin);
+[num_mc,options]=get_option(options,'num_mc',100);
+[block,options]=get_option(options,'block',100);
+[G,options]=get_option(options,'G',[]);
+check_unsupported_options(options,mfilename);
+
+err=0;
+nblocks=ceil(num_mc/block);
+xx=0;
+for j=1:nblocks
+    if j==nblocks
+        block=num_mc-block*(nblocks-1);
+    end
+    xi=randn(m,block);
+    u1=funcall( pce_func1, xi );
+    u2=funcall( pce_func2, xi );
+    du=u1-u2;
+    if isempty( G )
+        err=err+sum(sum(du.*du));
+    else
+        err=err+sum(sum(du.*(G*du)));
+    end
+    xx=xx+block;
+end
+if xx~=num_mc
+    keyboard;
+end
+err=sqrt(err/num_mc);
+
---
+++ unittest_pce_error_mc.m
@@ -0,0 +1,47 @@
+function unittest_pce_error_mc
+% UNITTEST_PCE_ERROR_MC Test the PCE_ERROR_MC function.
+%
+% Example (<a href="matlab:run_example unittest_pce_error_mc">run</a>)
+%   unittest_pce_error_mc
+%
+% See also PCE_ERROR_MC, TESTSUITE 
+
+%   Elmar Zander
+%   Copyright 2011, Inst. of Scientific Comuting
+%   $Id$ 
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+munit_set_function( 'pce_error_mc' );
+
+% set the random number generators to a specific state (rand for the random
+% generation of the coeffixients and randn for the monte carlo stuff)
+rand( 'seed', 12312 );
+randn( 'seed', 12314 );
+
+m=4;
+[ex, pce_func1, pce_func2]=setup( m, 0, 10 );
+assert_equals( ex, pce_error_mc( pce_func1, pce_func2, m, 'num_mc', 100, 'block', 1 ), 'p0b1');
+assert_equals( ex, pce_error_mc( pce_func1, pce_func2, m, 'num_mc', 100, 'block', 7 ), 'p0b7' );
+assert_equals( ex, pce_error_mc( pce_func1, pce_func2, m, 'num_mc', 100, 'block', 1000 ), 'p0b1000' );
+
+[ex, pce_func1, pce_func2]=setup( m, 2, 5 );
+assert_equals( ex, pce_error_mc( pce_func1, pce_func2, m ), 'p2', 'reltol', 0.1 );
+
+function [ex, pce_func1, pce_func2]=setup( m, p, N )
+I=multiindex(m,p);
+M=size(I,1);
+x_i_alpha=rand(N,M);
+y_i_alpha=rand(N,M);
+d_i_alpha=[x_i_alpha-y_i_alpha];
+[meand,vard]=pce_moments( d_i_alpha, I );
+ex=sqrt(sum(meand.^2+vard));
+
+pce_func1={ @pce_evaluate, {x_i_alpha, I}, {1,2} };
+pce_func2={ @pce_evaluate, {y_i_alpha, I}, {1,2} };
