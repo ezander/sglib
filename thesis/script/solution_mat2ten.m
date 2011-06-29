a150b7e236b362e5860471449ff29b76aa35b582
Elmar Zander <e.zander@tu-bs.de>
2011-06-29 19:46:11 +0200

    fixed solution conversion stuff

--- thesis/script/solution_mat2ten.m
+++ thesis/script/solution_mat2ten.m
@@ -1,8 +1,10 @@
 [ui_i_k,ui_k_alpha]=pce_to_kl( Ui_mat, I_u, inf, [], [] );
 Ui=kl_to_tensor(ui_i_k, ui_k_alpha);
 Ui=tensor_truncate( Ui, 'eps', 1e-14 );
-U=apply_boundary_conditions_solution( Ui, G, P_I, P_B );
+
+[u_i_k,u_k_alpha]=pce_to_kl( U_mat, I_u, inf, [], [] );
+U=kl_to_tensor(u_i_k, u_k_alpha);
+U=tensor_truncate( U, 'eps', 1e-14 );
 
 Ui_true=Ui;
 U_true=U;
-
--- thesis/script/solution_vec2mat.m
+++ thesis/script/solution_vec2mat.m
@@ -1,1 +1,2 @@
 Ui_mat=reshape( Ui_vec, [], M );
+U_mat=reshape( U_vec, [], M );
