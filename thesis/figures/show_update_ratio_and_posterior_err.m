773f76492cb8bd0c67f269c26fe03272190867ed
Elmar Zander <e.zander@tu-bs.de>
2011-11-25 16:24:24 +0100

    added plot for update ratio

--- thesis/figures/show_update_ratio_and_posterior_err.m
+++ thesis/figures/show_update_ratio_and_posterior_err.m
@@ -29,19 +29,20 @@ upratio_delta=1;
 cache_script solve_by_gsolve_tensor
 info.norm_U=gvector_norm(Ui);
 info.rho=rho
-plot_solution_overview( 'gsi_basic', info )
-%return
+plot_solution_overview( 'gsi_basic', info, 0 )
+plot_solution_overview( 'gsi_basic_10pc', info, 0.1 )
+plot_solution_overview( 'gsi_basic_1pc', info, 0.01 )
 
 
 solver_name='gpcg';
 cache_script solve_by_gsolve_tensor
 info.norm_U=gvector_norm(Ui);
 info.rho=rho
-plot_solution_overview( 'gpcg_basic', info )
+plot_solution_overview( 'gpcg_basic', info, false )
 
 
 
-function plot_solution_overview(model, info)
+function plot_solution_overview(model, info, delta)
 multiplot_init(1,1)
 multiplot;
 rho=info.rho;
@@ -51,9 +52,22 @@ while length(info.errvec)<length(info.resvec)
     info.errvec(end+1)=info.errvec(end);
 end
 plot( info.errvec, 'x-' ); legend_add( 'rel. error' );
-plot( errest, '*-' ); legend_add( 'error est.' );
-plot( info.resvec/info.resvec(1), 'o-' ); legend_add( 'rel. residual' );
+if delta==0
+    plot( errest, '*-' ); legend_add( 'error est.' );
+    plot( info.resvec/info.resvec(1), 'o-' ); legend_add( 'rel. residual' );
+end
 plot( info.updvec, 'd-' ); legend_add( 'update ratio' );
+
 logaxis( gca, 'y' )
+
+if delta
+    stop=find(abs(1-info.updvec)>delta, 1)
+    yl=ylim;
+    plot([stop,stop], yl, 'k--')
+    xl=xlim;
+    plot(xl, [1-delta, 1-delta], 'k--')
+    xlabel('k')
+end
+
 save_figure( gca, {'update_ratio_error_and_residual_%s', model} );
 
--- thesis/sparsity/show_sparsity_by_ordering.m
+++ thesis/sparsity/show_sparsity_by_ordering.m
@@ -2,14 +2,16 @@ function show_sparsity_by_ordering
 
 p_u=5;
 m_k=5; p_k=2;
-m_f=0; p_f=p_u;
+m_f=5; p_f=p_u;
 
 multiplot_init(2,2);
 
 for alphasort=[true, false]
-    S=compute_sparsity( m_k, p_k, m_f, p_f, p_u, alphasort, true, false );
-    
-    multiplot;
-    spy2(S);
-    save_figure( gca, {'sparsity_ordering_%d', alphasort} ) ;
+    for kfirst=[true, false]
+        S=compute_sparsity( m_k, p_k, m_f, p_f, p_u, alphasort, kfirst, false );
+        
+        multiplot;
+        spy2(S);
+        save_figure( gca, {'sparsity_ordering_%d', alphasort} ) ;
+    end
 end
