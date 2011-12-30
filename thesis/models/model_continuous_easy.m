05f4acb1fa907e483866bbdc2a8ed57bb26e4075
Elmar Zander <e.zander@tu-bs.de>
2011-12-30 12:12:24 +0100

    changed continuous model to lower var

--- thesis/models/model_continuous_easy.m
+++ thesis/models/model_continuous_easy.m
@@ -13,7 +13,7 @@ cov_f_func=@exponential_covariance;
 lc_f=1.8;
 
 % coefficient field
-[a,b]=beta_find_ratio( 0.2 );
+[a,b]=beta_find_ratio( 0.15 );
 dist_k={'beta', {a,b}, 0.001, 1.0 };
 cov_k_func=@gaussian_covariance;
 lc_k=1.3;
