c7a1416e4b1dce5e0ec523c060e72af269506e10
Elmar Zander <e.zander@tu-bs.de>
2012-01-03 00:35:12 +0100

    added solver_common_opts (missing from prev commit)

---
+++ thesis/script/solver_common_opts.m
@@ -0,0 +1,4 @@
+abstol=get_base_param( 'abstol', 1e-3, ws );
+reltol=get_base_param( 'reltol', 1e-3, ws );
+maxiter=get_base_param( 'maxiter', 100, ws );
+verbosity=get_base_param( 'verbosity', inf, ws );
