8ba2114cbb9053b67db6a92aa5ba232751db0570
Elmar Zander <e.zander@tu-bs.de>
2013-06-12 23:46:01 +0200

    Reactivated and updated the todo file

--- doc/todo.m
+++ doc/todo.m
@@ -1,28 +1,50 @@
+%% Todos as of 12 Jun 2013
+
+% Move paramstudy, octcompat into util
+% qmc -> sampling?
+% implement polysys_invcdf for sampling from qmc
+% implement statistics/xxx_invcdf for expansion of gpc parameters
+% implement all polysys distributions also in statistics (could reuse their
+% cdf then)
+% put kde into stats/private and use in kernel_estimate_fast or something
+% import stiffness matrix functions
+% remove thesis from here and organise as submodule thing
+
+% move plot_range, xxxspace into plotting?
+% check pcefunc stuff, works? 
+
+% make list of subdirs for and 
+%   check all Contents.m
+%   check lint stuff
+%   check for undocumented functions
+%   check for assert's 
+%   find funcs without unittest
+
+
+
+
+%% Old todo: need to check what's still relevant
+
 % some functions to write
-pce_field_expand
-pce_field_expand_gaussian
-pce_field_expand_lognormal?
-kl_pce_field_expand
-kl_pce_field_expand_gaussian
-kl_pce_moments->mean,var
+% pce_field_expand
+% pce_field_expand_gaussian
+% pce_field_expand_lognormal?
+% kl_pce_field_expand
+% kl_pce_field_expand_gaussian
+% kl_pce_moments->mean,var
+
+% preconditioners: use ilu, also invmean, and zabaras
+% examine number of iterations as function of prec type
 
 
-%
-preconditioners: use ilu, also invmean, and zabaras
-examine number of iterations as function of prec type
+% implement neumann bc
 
-
-% 
-implement neumann bc
-
-% 
-for cg: add tracking of res accuracy
+% for cg: add tracking of res accuracy
     
-%
-comparison with monte carlo (partly done)
-l2 norm diff with smolyak
+% comparison with monte carlo (partly done)
+% l2 norm diff with smolyak
 
 % parameter stuff
-get_param % whick workspace? separate functions? integration with autoloader; cache_script
+% get_param % whick workspace? separate functions? integration with autoloader; cache_script
 
 
