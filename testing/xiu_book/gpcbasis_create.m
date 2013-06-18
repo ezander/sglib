45bd0289457381bf8c40f787f3190cbb8b17ff74
Elmar Zander <e.zander@tu-bs.de>
2013-06-18 11:51:50 +0200

    Added file for demonstration of Section 3.1 (Orth. Polynomials) in Xiu2010

---
+++ testing/xiu_book/gpcbasis_create.m
@@ -0,0 +1,40 @@
+function V = gpcbasis_create(polysys, varargin)
+% GPCBASIS_CREATE Short description of gpcspace_create.
+%   GPCSPACE_CREATE Long description of gpcspace_create.
+%
+% Options
+%
+% References
+%
+% Notes
+%
+% Example (<a href="matlab:run_example gpcbasis_create">run</a>)
+%
+% See also
+
+%   Elmar Zander
+%   Copyright 2013, Inst. of Scientific Computing, TU Braunschweig
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+options=varargin2options(varargin);
+[m,options]=get_option(options, 'm', @isdefault);
+[p,options]=get_option(options, 'p', @isdefault);
+check_unsupported_options(options, mfilename);
+
+if isdefault(m)
+    m=length(polysys);
+end
+if isdefault(p)
+    p=0;
+end
+V = {polysys, multiindex(m, p)};
+
+function b=isdefault(p)
+b=isequal(p,@isdefault);
