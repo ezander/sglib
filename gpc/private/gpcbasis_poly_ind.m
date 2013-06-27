232d3867829b8113b1f87e23f63ef1f21cadbe27
Elmar Zander <e.zander@tu-bs.de>
2013-06-27 17:59:13 +0200

    Added gpcbasis_poly_ind for extraction for equal gpc 1d bases

---
+++ gpc/private/gpcbasis_poly_ind.m
@@ -0,0 +1,42 @@
+function inds=gpcbasis_poly_ind(V)
+% GPCBASIS_POLY_IND Extract information about equal 1d bases from GPC basis.
+%   INDS=GPCBASIS_POLY_IND(V) returns information about equal 1D orthogonal
+%   polynomial bases that occur in the GPC basis. This is used internally
+%   to speed up certain computations.
+%
+% Example (<a href="matlab:run_example gpcbasis_poly_ind">run</a>)
+%   V = gpcbasis_create('hlhlh');
+%   inds=gpcbasis_poly_ind(V);
+%   strvarexpand('$inds$');
+%
+% See also GPC
+
+%   Elmar Zander
+%   Copyright 2013, Inst. of Scientific Computing, TU Braunschweig
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+polysys = V{1};
+m=gpcbasis_size(V,2);
+
+if length(polysys)==1
+    inds = {{polysys, true(1,m)}};
+else
+    inds = {};
+    done = false(m,1);
+    for i=1:m
+        if done(i);
+            continue;
+        end
+        sys = polysys(i);
+        sys_ind = (polysys==sys);
+        inds{end+1} = {sys, sys_ind}; %#ok<AGROW>
+        done(sys_ind) = true;
+    end
+end
---
+++ gpc/private/unittest_gpcbasis_poly_ind.m
@@ -0,0 +1,30 @@
+function unittest_gpcbasis_poly_ind
+% UNITTEST_GPCBASIS_POLY_IND Test the GPCBASIS_POLY_IND function.
+%
+% Example (<a href="matlab:run_example unittest_gpcbasis_poly_ind">run</a>)
+%   unittest_gpcbasis_poly_ind
+%
+% See also GPCBASIS_POLY_IND, TESTSUITE 
+
+%   Elmar Zander
+%   Copyright 2013, Inst. of Scientific Computing, TU Braunschweig
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
+munit_set_function( 'gpcbasis_poly_ind' );
+
+
+
+V = gpcbasis_create('h', 'm', 3);
+inds=gpcbasis_poly_ind(V);
+assert_equals(inds, {{'h', true(1,3)}}, 'same');
+
+V = gpcbasis_create('PlPPlP');
+inds=gpcbasis_poly_ind(V);
+assert_equals(inds, {{'P', logical([1,0,1,1,0,1])},{'l', logical([0,1,0,0,1,0])}}, 'two');
