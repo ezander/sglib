50a1a2806f45fcacdde388eb1df5a24fa1dc1167
Elmar Zander <e.zander@tu-bs.de>
2014-03-05 20:05:28 +0100

    work on the quasi newton framework

--- tensor/tensor_size.m
+++ tensor/tensor_size.m
@@ -1,14 +1,19 @@
-function s=tensor_size(T)
+function s=tensor_size(T, contract)
 % TENSOR_SIZE Returns the size of the tensor.
-%   S=TENSOR_SIZE(T) returns the size of the tensor T in the vector D. If
+%   S=TENSOR_SIZE(T) returns the size of the tensor T in the vector S. If
 %   the tensor if of order D>=2, then S has length D. Only for order 0 or 1
 %   S has always length 2;
+%
+%   S=TENSOR_SIZE(T, TRUE) returns the linear size of the tensor T in S. If
+%   the tensor has order D>=2, the product of the dimensions is returned,
+%   such that S always is a scalar.
 %
 % Example (<a href="matlab:run_example tensor_size">run</a>)
 %   T = rand(3,5,7);
 %   tensor_size(T)
 %   T = {rand(3,10), rand(5,10), rand(7,10)};
 %   tensor_size(T)
+%   tensor_size(T, true)
 %   T = tensor_to_vector(T);
 %   tensor_size(T)
 %
@@ -25,6 +30,10 @@ function s=tensor_size(T)
 %   received a copy of the GNU General Public License along with this
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
+if nargin<2
+    contract=false;
+end
+
 if isnumeric(T)
     s=size(T);
 elseif is_ctensor(T)
@@ -34,4 +43,8 @@ elseif isobject(T)
 else
     error( 'sglib:param_error', ...
         'input parameter is no recognized tensor format' );
+end
+
+if contract
+    s=prod(s);
 end
