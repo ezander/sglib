6cb963e107092cc16d51e7f8da59d72fe14396f1
Elmar Zander <e.zander@tu-bs.de>
2014-03-11 15:00:03 +0100

    Added comparison with bfgs-nonintrusive

--- solver/lbfgs_solve.m
+++ solver/lbfgs_solve.m
@@ -1,6 +1,14 @@
 function r = lbfgs_solve(H0, Y, S, q)
 
-k = length(S);
+if isempty(H0)
+    k0 = length(S);
+    %k = k0 - 1;
+    k = k0;
+else
+    k = length(S);
+end
+
+
 alpha = nan(1, k);
 beta = nan(1, k);
 rho = nan(1, k);
@@ -9,7 +17,16 @@ for i=k:-1:1
     alpha(i) = rho(i) * tensor_scalar_product(S{i}, q);
     q = tensor_add(q, Y{i}, -alpha(i));
 end
-r = H0 * q;
+if isempty(H0)
+    if k0>0
+        tau = tensor_scalar_product(Y{k0}, S{k0}) / tensor_scalar_product(Y{k0}, Y{k0});
+        r = tensor_scale(q, tau);
+    else
+        r = q;
+    end
+else
+    r = operator_apply(H0, q);
+end
 for i=1:k
     beta(i) = rho(i) * tensor_scalar_product(Y{i}, r);
     r = tensor_add(r, S{i}, alpha(i)-beta(i));
