27ff32e71bceeff4546b9e2dd436df5f2be853eb
Elmar Zander <e.zander@tu-bs.de>
2015-11-20 16:06:16 +0100

    Unittest for ParamSet and restructuring

--- objects/unittest_SimParamSet.m
+++ objects/unittest_SimParamSet.m
@@ -17,5 +17,26 @@ function unittest_SimParamSet(varargin)
 %   received a copy of the GNU General Public License along with this
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
+%%
 munit_set_function( 'SimParamSet' );
 
+clear all
+Q = SimParamSet();
+Q.add('foo', NormalDistribution(2,3));
+Q.add('bar', ExponentialDistribution(4));
+Q.add('baz', LogNormalDistribution(1,1.2));
+Q.add('quux', UniformDistribution(3,7), 'plot_name', 'QuuX');
+Q.set_fixed('baz', 10)
+
+assert_equals(Q.num_params, 4, 'num_params');
+assert_equals(Q.find_ind_rv, logical([1;1;0;1]), 'ind_rv');
+assert_equals(Q.num_rv, 3, 'num_rvs');
+
+assert_equals(Q.param_names, {'foo', 'bar', 'baz', 'quux'}', 'params_names');
+assert_equals(Q.param_plot_names, {'foo', 'bar', 'baz', 'QuuX'}', 'plot_names');
+assert_equals(Q.rv_names, {'foo', 'bar', 'quux'}', 'rv_names');
+assert_equals(Q.rv_plot_names, {'foo', 'bar', 'QuuX'}', 'rv_plot_names');
+
+
+
+%[Q_alpha, V_Q, varserr]=gpc_expand_RVs(Q)
