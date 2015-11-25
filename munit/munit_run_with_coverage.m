7a17549e1047d5f766885007742a6e4931ceaf7b
Elmar Zander <e.zander@tu-bs.de>
2015-11-25 16:40:44 +0100

    munit_run_with_coverage now first finds the path the test resides in

--- munit/munit_run_with_coverage.m
+++ munit/munit_run_with_coverage.m
@@ -19,13 +19,29 @@ function munit_run_with_coverage(func_or_unittest)
 %   This program is free software: you can redistribute it and/or modify it
 %   under the terms of the GNU General Public License as published by the
 %   Free Software Foundation, either version 3 of the License, or (at your
-%   option) any later version. 
+%   option) any later version.
 %   See the GNU General Public License for more details. You should have
 %   received a copy of the GNU General Public License along with this
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
 if nargin>0
-    munit_run_testsuite('coverage', true, 'include_only', {func_or_unittest}, 'subdirs', {});
+    pathstr=find_containing_dir(func_or_unittest);
+    munit_run_testsuite('coverage', true, 'dir', pathstr, 'include_only', {func_or_unittest}, 'subdirs', {});
 else
     munit_run_testsuite('coverage', true);
+end
+
+function pathstr=find_containing_dir(func_or_unittest)
+% FIND_CONTAINING_DIR Finds the directory that contains the test.
+prefix = munit_options('get', 'prefix');
+if strncmp(func_or_unittest, prefix, length(prefix))
+    func = func_or_unittest(length(prefix)+1:end);
+else
+    func = func_or_unittest;
+end
+unittest = [prefix func];
+if ~exist(['./' unittest '.m'], 'file')
+    pathstr = fileparts(which(unittest));
+else
+    pathstr = pwd;
 end
