1c0c77b1539d7271cce7c70a93b352bcef75db05
noemi <n.friedman@tu-bs.de>
2016-01-22 11:22:19 +0100

    Small changes for getting started with Kalman Filter (stdnor funcitons in the Distribution and MySimParamSet)

--- objects/compute_measurement.m
+++ objects/compute_measurement.m
@@ -1,5 +1,12 @@
 function y_j_k = compute_measurement(xi_i_k, g_func, Q)
-q_i_k=Q.germ2params(xi_i_k);
+options=varargin2options(varargin);
+[is_xi_stdnor,options]=get_option(options, 'is_xi_stdnor', false);
+check_unsupported_options(options,mfilename);
+if is_xi_stdnor
+    q_i_k=Q.stdnor2params(xi_i_k);
+else
+    q_i_k=Q.germ2params(xi_i_k);
+end
 L = size(xi_i_k, 2);
 
 for k=1:L
