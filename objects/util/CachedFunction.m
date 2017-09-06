ac8559b17359333f01526b66cc40bd444824f3af
Elmar Zander <e.zander@tu-bs.de>
2017-09-06 14:14:43 +0200

    Made the CachedFunction a handle class and improved some stuff

--- objects/util/CachedFunction.m
+++ objects/util/CachedFunction.m
@@ -1,10 +1,15 @@
-classdef CachedFunction
+classdef CachedFunction < handle
     properties
         result_cache@containers.Map
         cached_function
         cache_filename
         verbosity = 0;
         version_tag = 0;
+        
+        last_save_time = -inf;
+        %always_save = true;
+        always_save = false;
+        unsaved_changes = false;
     end
     
     
@@ -40,7 +45,15 @@ classdef CachedFunction
         
         function save_cache_file(filename, cache, version_tag, verbosity)
             % SAVE_CACHE_FILE Save the cache to a file
-            save(filename, 'cache', 'version_tag');
+            if verbosity>=2
+                strvarexpand('Saving cache file "$filename$" ($datestr(now)$');
+            end
+            t=tic;
+            save(filename, 'cache', 'version_tag', '-v7.3');
+            dt=toc(t);
+            if verbosity>=2 || (verbosity > 0 && dt>1)
+                strvarexpand('Time for writing to cache file "$filename$": $dt$');
+            end
         end
     end
     
@@ -54,9 +67,21 @@ classdef CachedFunction
             
             cfunc.cache_filename = filename;
             cfunc.result_cache = cfunc.load_cache_file(filename, version_tag, cfunc.verbosity);
+            cfunc.unsaved_changes = false;
+            cfunc.last_save_time = now();
+            
             cfunc.cached_function = func;
             cfunc.version_tag = version_tag;
             cfunc.verbosity = verbosity;
+        end
+        
+        function delete(cfunc)
+            % DELETE Class destructor, should save the file, if not done already.
+            if cfunc.unsaved_changes
+                cfunc.save_cache_file(cfunc.cache_filename, ...
+                    cfunc.result_cache, cfunc.version_tag, cfunc.verbosity);
+                cfunc.unsaved_changes = false;
+            end
         end
         
         function result=call(cfunc, varargin)
@@ -69,11 +94,25 @@ classdef CachedFunction
             end
         end
         
+        function do_save(cfunc)
+            cfunc.save_cache_file(cfunc.cache_filename, ...
+                cfunc.result_cache, cfunc.version_tag, cfunc.verbosity);
+            cfunc.last_save_time = now();
+            cfunc.unsaved_changes = false;
+        end
+        
         function store(cfunc, args, result)
             hashstr=cfunc.get_hashstr(args);
             cfunc.result_cache(hashstr) = {result, args};
-            cfunc.save_cache_file(cfunc.cache_filename, ...
-                cfunc.result_cache, cfunc.version_tag, cfunc.verbosity);
+            
+            max_dt_in_minutes = 0.5;
+            max_dt = (max_dt_in_minutes * 60) / (60*60*24);
+            save_now = cfunc.always_save || cfunc.last_save_time<now - max_dt;
+            if save_now
+                cfunc.do_save();
+            else
+                cfunc.unsaved_changes = true;
+            end
         end
         
         function [result, found, origargs] = retrieve(cfunc, args)
